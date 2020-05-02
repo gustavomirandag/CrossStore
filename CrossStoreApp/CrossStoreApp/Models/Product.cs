7f27c30a1920cfbde429662d24b93a4a2761b0f2
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-05-02 20:11:14 -0300

    Add project files.

---
+++ CrossStoreApp/CrossStoreApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossStoreApp.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Photo { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
